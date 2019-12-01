c6418111edd0432b034a8911111c3be413737e83
Ashlyn Baum <ashlyn@traject.nz>
2019-12-01 17:37:49 +1300

    add animated arrow with react spring

---
+++ src/components/arrow-icon.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import {Spring, animated, config} from 'react-spring/renderprops'
+
+export default ({ data }) => {
+
+  return (
+    <Spring
+      native
+      reset
+      delay={1000}
+      from={{ x: 100, opacity: 0 }}
+      to={{ x: 0, opacity: 1 }}
+      config={config.molasses}
+    >
+      {({ x, opacity }) => (
+        <animated.svg
+          className={"icon"}
+          style={{ opacity }}
+          // viewBox="0 0 24 24"
+          fill="none"
+          stroke="black"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeDasharray={100}
+          strokeDashoffset={x}
+          width="16" 
+          height="47" 
+          // viewBox="0 0 16 47"
+        >
+          <path pathLength="100" d="M6.5 45.5V5.5"  />
+          <path pathLength="100" d="M6.5 45.5L12.5 40.5"  />
+          <path pathLength="100" d="M6.5 45.5L0.5 40.5" />
+          
+
+        </animated.svg>
+      )}
+    </Spring>
+  )
+}
--- src/components/hero.js
+++ src/components/hero.js
@@ -3,38 +3,41 @@ import AshlynBanner from './assets/ashlyn-banner-v2.svg'
 import styles from './hero.module.css'
 import SubscribeForm from './hero-form.js'
 import Squiggles from './assets/squiggles.svg'
-import animation from './animation.css'
-import Navigation from '../components/navigation'
+import ArrowIcon from './arrow-icon.js'
 
 
 
 
-export default ({ data }) => (
-  <div>
-    <div className={styles.shaddow}>
-      {/* <Navigation /> */}
-      <div className={styles.hero}>
-        <AshlynBanner className={styles.ashlynBanner}/>
-        <div className={styles.heroRight}>
-          <div>
-            <div className={styles.websiteName}>ashlyn.app</div>
-          </div>
-          <div>
-            <h1 className={styles.heroHeadline}>handbooks</h1>
-            <div className={styles.heroSubtitle}>
-              <div>for building good digital products</div>
-              <div> and other things</div>
+export default ({ data }) => {
+
+
+  return (
+    <div>
+      <div className={styles.shaddow}>
+        {/* <Navigation /> */}
+        <div className={styles.hero}>
+          <AshlynBanner className={styles.ashlynBanner}/>
+          <div className={styles.heroRight}>
+            <div>
+              <div className={styles.websiteName}>ashlyn.app</div>
             </div>
+            <div>
+              <h1 className={styles.heroHeadline}>handbooks</h1>
+              <div className={styles.heroSubtitle}>
+                <div>for building good digital products</div>
+                <div> and other things</div>
+              </div>
+            </div>
+            <SubscribeForm></SubscribeForm>
           </div>
-          <SubscribeForm></SubscribeForm>
+          <Squiggles className={styles.squiggles} />
         </div>
-        <Squiggles className={styles.squiggles} />
+      </div>
+      <div className={styles.arrowWrapper}>
+        <ArrowIcon />
       </div>
     </div>
-    <div>
-      <div>arrow</div>
-      <div>get started</div>
-    </div>
-  </div>
-)
+  ) 
+
+}
 
