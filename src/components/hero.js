c6f610dbd631930695c2da885ae9d7d594d9cdaa
Ashlyn Baum <ashlyn@traject.nz>
2019-12-05 18:56:55 +1300

    parralax and handbook view

--- src/components/hero.js
+++ src/components/hero.js
@@ -3,34 +3,40 @@ import AshlynBanner from './assets/ashlyn-banner-v2.svg'
 import styles from './hero.module.css'
 import SubscribeForm from './hero-form.js'
 import Squiggles from './assets/squiggles.svg'
+import {useSpring, animated} from 'react-spring'
 
 // import {Parallax, ParallaxLayer} from 'react-spring/renderprops-addons'
 
 
 
 export default ({ data }) => {
+  const props = useSpring({height: '90vh', from: {height: '110vh'}})
 
   return (
     <div>
       <div className={styles.shaddow}>
         {/* <Navigation /> */}
-        <div className={styles.hero}>
-          <AshlynBanner className={styles.ashlynBanner}/>
-          <div className={styles.heroRight}>
-            <div>
-              <div className={styles.websiteName}>ashlyn.app</div>
+        <animated.div className={styles.hero} style={props}>
+          <div className={styles.innerWrapper}>
+            <div className={styles.heroLeft}>
+              <AshlynBanner className={styles.ashlynBanner}/>
             </div>
-            <div>
-              <h1 className={styles.heroHeadline}>handbooks</h1>
-              <div className={styles.heroSubtitle}>
-                <div>for building good digital products</div>
-                <div> and other things</div>
+            <div className={styles.heroRight}>
+              <div className={styles.websiteNameWrapper}>
+                <div className={styles.websiteName}>ashlyn.app</div>
               </div>
+              <div>
+                <h1 className={styles.heroHeadline}>handbooks</h1>
+                <div className={styles.heroSubtitle}>
+                  <div>for building good digital products</div>
+                  <div> and other things</div>
+                </div>
+              </div>
+              <SubscribeForm></SubscribeForm>
             </div>
-            <SubscribeForm></SubscribeForm>
+            <Squiggles className={styles.squiggles} />
           </div>
-          <Squiggles className={styles.squiggles} />
-        </div>
+        </animated.div>
       </div>
     </div>
   ) 
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,13 +5,18 @@ import Helmet from 'react-helmet'
 import Hero from '../components/hero'
 import Layout from '../components/layout'
 import BookCover from '../components/book-cover'
-import AshlynBanner from '../components/assets/ashlyn-banner-v2.svg'
 import HandbookPreview from '../components/handbook-preview'
 import {Parallax, ParallaxLayer} from 'react-spring/renderprops-addons'
-import Blob1 from '../components/assets/blob1.svg'
-import Blob2 from '../components/assets/blob2.svg'
 import ArrowIcon from '../components/arrow-icon.js'
 import styles from '../components/hero.module.css'
+import AshlynBanner from '../components/assets/ashlyn-banner-v2.svg'
+
+// import svgs
+import Blob1 from '../components/assets/blob1.svg'
+import Blob2 from '../components/assets/blob2.svg'
+import Blob3 from '../components/assets/blob3.svg'
+import Box from '../components/assets/box.svg'
+import Mockup from '../components/assets/mockup.svg'
 
 class RootIndex extends React.Component {
   render() {
@@ -21,9 +26,9 @@ class RootIndex extends React.Component {
     
     return (
     <Parallax pages={4} ref={ref => (this.parallax = ref)}>
-      {/* <ParallaxLayer offset={0} speed={0.5}>
+      <ParallaxLayer offset={0} speed={0.5}>
         <span onClick={() => this.parallax.scrollTo(1)}>Layers can contain anything</span>
-      </ParallaxLayer> */}
+      </ParallaxLayer>
 
 
       <Layout location={this.props.location} >
@@ -34,42 +39,53 @@ class RootIndex extends React.Component {
 
           <ParallaxLayer offset={0} speed={1} factor={1}>
             <Hero />
-            <span onClick={() => this.parallax.scrollTo(1)}>
+            <span onClick={() => this.parallax.scrollTo(.8)}>
                 <ArrowIcon />
             </span>
           </ParallaxLayer>
 
-          <ParallaxLayer offset={1.2} speed={-0.2} style={{ pointerEvents: 'none' }}>
+          {/* <ParallaxLayer offset={1.2} speed={-0.2} style={{ pointerEvents: 'none' }}>
             <Blob1 />
           </ParallaxLayer>
 
           <ParallaxLayer offset={1.5} speed={0} style={{ pointerEvents: 'none' }}>
             <Blob2 />
-          </ParallaxLayer>
+          </ParallaxLayer> */}
 
           {/* <ParallaxLayer offset={0} speed={-1} factor={.5}>
             <AshlynBanner className={styles.ashlynBanner}/>
           </ParallaxLayer> */}
 
-
+          <ParallaxLayer offset={1} speed={1.4}>
+            <Box style={{marginLeft: "8%"}}/>
+          </ParallaxLayer>
+          <ParallaxLayer offset={1.3} speed={-0.2} factor={3}>
+            <Mockup style={{marginLeft: '15%'}}/>
+          </ParallaxLayer>
+          <ParallaxLayer offset={2} speed={1} factor={1}>
+            <Blob3 style={{marginLeft: '60%'}}/>
+          </ParallaxLayer>
+          {/* <ParallaxLayer offset={1} speed={-1} factor={.5}>
+          </ParallaxLayer> */}
           
-          <ParallaxLayer offset={1.4} speed={-0.1}>
-            <h2>Strategies for you and your team to build products that people love.</h2>
-            <span onClick={() => this.parallax.scrollTo(2)}>
+          <ParallaxLayer offset={1.3} speed={-0.1}>
+            <h2 style={{marginLeft: '50%'}}>Strategies to build products that people love</h2>
+            {/* <span onClick={() => this.parallax.scrollTo(2)}>
                 <ArrowIcon />
-            </span>
+            </span> */}
           </ParallaxLayer>
           <ParallaxLayer offset={2} speed={0.5}>
             <div className="wrapper">
 
-
-              <BookCover />
-              {/* link to product - using workbook/blog pages 
-              passing through product handbook
-              link to slug on welcome*/}
-              <div className="article-list">
-                <div>
-                  <HandbookPreview handbook={productFeature} name="Product Feature Development" description="Building great products one feature at a time."/>
+              <div className="HandbookSection" style={{display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-around'}}>
+                <BookCover />
+                {/* link to product - using workbook/blog pages 
+                passing through product handbook
+                link to slug on welcome*/}
+                <div className="article-list" style={{margin: '6%'}}>
+                  <div>
+                    <HandbookPreview handbook={productFeature} name="Product Feature Development" description="Building great products one feature at a time."/>
+                  </div>
                 </div>
               </div>
             </div>
