3e291240782a39618bbab12493d0be2d13250415
Ashlyn Baum <ashlyn@traject.nz>
2020-01-03 15:47:02 -0600

    set active link on handbook navigation, but hardcoded handbook url instead of reading location from URL.

--- src/components/handbook-nav-item.js
+++ src/components/handbook-nav-item.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import styles from './navigation.module.css'
+import styles from './handbook-nav-list.module.css'
 
 export default ({ item, link }) => {
+    console.log("path", link)
     return (
     <li>
-        <Link to={`${location.pathname}/../${link}`} partiallyActive={true}>
+        <Link to={`/product-handbook/${link}`} activeClassName={styles.active}>
             {item}
         </Link>
     </li>);
