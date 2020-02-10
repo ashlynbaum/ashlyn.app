6bd7700d93227407fa128e520962be39c389083f
Ashlyn Baum <ashlyn@traject.nz>
2020-02-10 15:12:56 +1300

    add react get screen package to check screen size, implement on index page, update hero styling based on screen size.

--- src/components/hero.js
+++ src/components/hero.js
@@ -11,32 +11,32 @@ import Animation from './animation.css'
 
 
 
-export default ({ data }) => {
+export default ({ data, isMobile }) => {
   const props = useSpring({height: '90vh', from: {height: '110vh'}})
 
   return (
     <div>
       <div className={styles.shaddow}>
         {/* <Navigation /> */}
-        <animated.div className={styles.hero} style={props}>
-          <div className={styles.innerWrapper}>
+        <animated.div className={isMobile ? styles.heroMobile : styles.hero} style={props}>
+          <div className={isMobile ? styles.innerWrapperMobile : styles.innerWrapper}>
             <div className={styles.heroLeft}>
-              <AshlynBanner className={styles.ashlynBanner}/>
+              <AshlynBanner className={isMobile ? styles.ashlynBannerMobile : styles.ashlynBanner}/>
             </div>
-            <div className={styles.heroRight}>
+            <div className={isMobile ? styles.heroRightMobile : styles.heroRight}>
               <div className={styles.websiteNameWrapper}>
                 <div className={styles.websiteName}>ashlyn.app</div>
               </div>
               <div>
                 <h1 className={styles.heroHeadline}>handbooks</h1>
-                <div className={styles.heroSubtitle}>
+                <div className={isMobile ? styles.heroSubtitleMobile : styles.heroSubtitle}>
                   <div>for building good digital products</div>
                   <div> and other things</div>
                 </div>
               </div>
               <SubscribeForm></SubscribeForm>
             </div>
-            <Squiggles className={styles.squiggles} />
+            <Squiggles className={isMobile ? styles.squigglesMobile :styles.squiggles} />
           </div>
         </animated.div>
       </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { withGetScreen } from 'react-getscreen'
 import { graphql } from 'gatsby'
 import get from 'lodash/get'
 import Helmet from 'react-helmet'
@@ -10,15 +11,9 @@ import {Parallax, ParallaxLayer} from 'react-spring/renderprops-addons'
 import ArrowIcon from '../components/arrow-icon.js'
 import Footer from '../components/footer.js'
 import styles from './index.module.css'
-import AshlynBanner from '../components/assets/ashlyn-banner-v2.svg'
-import StickerCard from '../components/sticker-card.js'
 
 
 // import svgs
-import Blob1 from '../components/assets/blob1.svg'
-import Blob2 from '../components/assets/blob2.svg'
-import Blob3 from '../components/assets/blob3.svg'
-import Mockup from '../components/assets/mockup.svg'
 import AshlynPerson from '../components/assets/ashlyn-lounge-person.svg'
 import AshlynPlants from '../components/assets/ashlyn-lounge-plants.svg'
 import CheckCircle from '../components/assets/check-circle.svg'
@@ -27,36 +22,25 @@ class RootIndex extends React.Component {
   render() {
     const handbooks = get(this, 'props.data.allSourceConfig.nodes')
     const productFeature = get(this, 'props.data.allProduct')
-
-    
-    return (
-    <Parallax pages={4} ref={ref => (this.parallax = ref)}>
-      {/* <ParallaxLayer offset={0} speed={0.5}>
-        <span onClick={() => this.parallax.scrollTo(1)}>Layers can contain anything</span>
-      </ParallaxLayer> */}
-
-
-      <Layout location={this.props.location}>
+   
+    // mobile version
+    if (this.props.isMobile()) 
+      return (
+        <Layout location={this.props.location}>
 
         <div>
           <Helmet />
 
-
-          <ParallaxLayer offset={0} speed={0.5}>
-            <Hero />
+            <Hero isMobile={true}/>
             <span onClick={() => this.parallax.scrollTo(.8)}>
                 <ArrowIcon />
             </span>
-          </ParallaxLayer>
 
-          <ParallaxLayer offset={1} speed={0} factor={1} style={{ pointerEvents: 'none' }}>
             <div className={styles.benefitBox}></div>
-          </ParallaxLayer>
 
 
           <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
             <div style={{width: '40%', margin: '0 0 0 15%'}}>
-              <ParallaxLayer offset={1.1} speed={0.2} factor={1}>
                 <div className="benefit" style={{width: '40%'}}>
                     <h2>Build products that people love</h2>
                     <p>Hanbooks have strategies for product and business opperations. <a>ashlyn.app</a> handbooks are a sharable resources for you to build best practice in your team. 
@@ -70,85 +54,144 @@ class RootIndex extends React.Component {
                 <span onClick={() => this.parallax.scrollTo(1.6)}>
                   {/* <ArrowIcon /> */}
                 </span>
-              </ParallaxLayer>
             </div>
             <div style={{width: '40%', margin: '0 5% 0 0'}}>
-              <ParallaxLayer offset={1.05} speed={0.8} factor={.2}>
                 <AshlynPlants style={{marginTop: "-10%"}}/>
-              </ParallaxLayer>
-              <ParallaxLayer offset={1.15} speed={.2} factor={.2}>
                 <AshlynPerson style={{marginTop: "-10%"}}/>
-              </ParallaxLayer>
             </div>
 
           </div>
+        </div>
+      </Layout>
+      );
 
-          {/* <ParallaxLayer offset={1.7} speed={.2}>
-              <Blob3 style={{marginLeft: '60%'}}/>
-          </ParallaxLayer> */}
+    // -----
+      // tablet version
+    // if (this.props.isTablet()) 
+      // return (  <div>Tablet</div> ) ;
+    // ----
+    //desktop version
 
-
-
-          {/* <ParallaxLayer offset={2.1} speed={0} factor={1} style={{ pointerEvents: 'none' }}>
-            <div className={styles.box}></div>
-          </ParallaxLayer> */}
-
-          <ParallaxLayer offset={2} speed={-.2} factor={1} style={{ pointerEvents: 'none' }}>
-            <div className={styles.boxLine}></div>
-          </ParallaxLayer>
-
-          <ParallaxLayer offset={2} speed={1.1} factor={1} style={{ pointerEvents: 'none' }}>
-            <div className={styles.boxLine2}></div>
-          </ParallaxLayer>
-          <ParallaxLayer offset={2.05} speed={1.7} factor={1} style={{ marginLeft: '5%', pointerEvents: 'none', transform: 'scale(0.7)' }}>
-            <div className={styles.boxLine}></div>
-          </ParallaxLayer>
-          <ParallaxLayer offset={2.1} speed={-.2} factor={1} style={{ pointerEvents: 'none' }}>
-            <div className={styles.benefitBox}></div>
-          </ParallaxLayer>
+      return (
+      <Parallax pages={4} ref={ref => (this.parallax = ref)}>
+        {/* <ParallaxLayer offset={0} speed={0.5}>
+          <span onClick={() => this.parallax.scrollTo(1)}>Layers can contain anything</span>
+        </ParallaxLayer> */}
 
 
+        <Layout location={this.props.location}>
 
-          {/* <ParallaxLayer offset={2.3} speed={0.15} factor={1} style={{ pointerEvents: 'none' }}>
-            <div className={styles.box2}></div>
-          </ParallaxLayer> */}
+          <div>
+            <Helmet />
 
-          <ParallaxLayer offset={1.86} speed={0} factor={1}>
-            <div className="wrapper-handbooks">
 
-              <div className="handbook-section">
-                <BookCover />
-                {/* link to product - using workbook/blog pages 
-                passing through product handbook
-                link to slug on welcome*/}
-                <div className="article-list">
-                  <div>
-                    <HandbookPreview handbook={productFeature} name="Product Development" description="Building great products one feature at a time."/>
+            <ParallaxLayer offset={0} speed={0.5}>
+              <Hero />
+              <span onClick={() => this.parallax.scrollTo(.8)}>
+                  <ArrowIcon />
+              </span>
+            </ParallaxLayer>
+
+            <ParallaxLayer offset={1} speed={0} factor={1} style={{ pointerEvents: 'none' }}>
+              <div className={styles.benefitBox}></div>
+            </ParallaxLayer>
+
+
+            <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+              <div style={{width: '40%', margin: '0 0 0 15%'}}>
+                <ParallaxLayer offset={1.1} speed={0.2} factor={1}>
+                  <div className="benefit" style={{width: '40%'}}>
+                      <h2>Build products that people love</h2>
+                      <p>Hanbooks have strategies for product and business opperations. <a>ashlyn.app</a> handbooks are a sharable resources for you to build best practice in your team. 
+                      </p>
+                      {/* <ul>
+                        <li><CheckCircle /><span>Create shared language</span></li>
+                        <li><CheckCircle /><span>Clear process</span></li>
+                        <li><CheckCircle /><span>Follow best practice</span></li>
+                      </ul> */}
+                  </div>
+                  <span onClick={() => this.parallax.scrollTo(1.6)}>
+                    {/* <ArrowIcon /> */}
+                  </span>
+                </ParallaxLayer>
+              </div>
+              <div style={{width: '40%', margin: '0 5% 0 0'}}>
+                <ParallaxLayer offset={1.05} speed={0.8} factor={.2}>
+                  <AshlynPlants style={{marginTop: "-10%"}}/>
+                </ParallaxLayer>
+                <ParallaxLayer offset={1.15} speed={.2} factor={.2}>
+                  <AshlynPerson style={{marginTop: "-10%"}}/>
+                </ParallaxLayer>
+              </div>
+
+            </div>
+
+            {/* <ParallaxLayer offset={1.7} speed={.2}>
+                <Blob3 style={{marginLeft: '60%'}}/>
+            </ParallaxLayer> */}
+
+
+
+            {/* <ParallaxLayer offset={2.1} speed={0} factor={1} style={{ pointerEvents: 'none' }}>
+              <div className={styles.box}></div>
+            </ParallaxLayer> */}
+
+            <ParallaxLayer offset={2} speed={-.2} factor={1} style={{ pointerEvents: 'none' }}>
+              <div className={styles.boxLine}></div>
+            </ParallaxLayer>
+
+            <ParallaxLayer offset={2} speed={1.1} factor={1} style={{ pointerEvents: 'none' }}>
+              <div className={styles.boxLine2}></div>
+            </ParallaxLayer>
+            <ParallaxLayer offset={2.05} speed={1.7} factor={1} style={{ marginLeft: '5%', pointerEvents: 'none', transform: 'scale(0.7)' }}>
+              <div className={styles.boxLine}></div>
+            </ParallaxLayer>
+            <ParallaxLayer offset={2.1} speed={-.2} factor={1} style={{ pointerEvents: 'none' }}>
+              <div className={styles.benefitBox}></div>
+            </ParallaxLayer>
+
+
+
+            {/* <ParallaxLayer offset={2.3} speed={0.15} factor={1} style={{ pointerEvents: 'none' }}>
+              <div className={styles.box2}></div>
+            </ParallaxLayer> */}
+
+            <ParallaxLayer offset={1.86} speed={0} factor={1}>
+              <div className="wrapper-handbooks">
+
+                <div className="handbook-section">
+                  <BookCover />
+                  {/* link to product - using workbook/blog pages 
+                  passing through product handbook
+                  link to slug on welcome*/}
+                  <div className="article-list">
+                    <div>
+                      <HandbookPreview handbook={productFeature} name="Product Development" description="Building great products one feature at a time."/>
+                    </div>
                   </div>
                 </div>
               </div>
+            </ParallaxLayer>
+
+          </div>
+          {/* <ParallaxLayer offset={2.3} speed={-0.1}>
+            <div className={styles.stickerWrapper}>
+              <div><p>shop</p></div>
+              <StickerCard />
             </div>
+          </ParallaxLayer> */}
+
+          <ParallaxLayer offset={3.4} speed={-0.1}>
+            <Footer />
           </ParallaxLayer>
 
-        </div>
-        {/* <ParallaxLayer offset={2.3} speed={-0.1}>
-          <div className={styles.stickerWrapper}>
-            <div><p>shop</p></div>
-            <StickerCard />
-          </div>
-        </ParallaxLayer> */}
-
-        <ParallaxLayer offset={3.4} speed={-0.1}>
-          <Footer />
-        </ParallaxLayer>
-
-      </Layout>
-    </Parallax>
-    )
+        </Layout>
+      </Parallax>
+      )
   }
 }
 
-export default RootIndex
+export default withGetScreen(RootIndex, { mobileLimit: 768})
 
 export const query =  graphql`
 {
